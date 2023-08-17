7de99c1d1f865a68134b67baea07a4176da4a4f7
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-08-17 15:08:48 +0100

    code tidy

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ Future dev to include other media plus links to websites and simple text notes.
 This may require OAuth 2.0 set up to use? Will be fun adding google login to this app.
 API / OAuth not required for search function.
 
-Current status, hard coded search and render to screen. Next step is to add a search bar and input field.
-Then add a save button to select the book required and save to the list.
+### Current state
+
+Add a save button to select the book required and save to the list.
 
 FInal step link to firebase DB to save the list. (live updates)
--- app.js
+++ app.js
@@ -1,17 +1,6 @@
 // build a quick test div to see if the app is working
 const searchResultsElement = document.getElementById("foundBooks");
 
-// test api call to google.books api
-
-const url = "https://www.googleapis.com/books/v1/volumes?q=";
-
-const getBooks = async (search) => {
-  console.log(url + search + "&maxResults=5");
-  const res = await fetch(url + search + "&maxResults=5");
-  const data = await res.json();
-  return data.items;
-};
-
 // function to get user input and save as search string
 const getInput = () => {
   const searchInput = document.getElementById("searchInput").value;
@@ -21,13 +10,22 @@ const getInput = () => {
     return;
   }
 
-  render(searchInput);
+  return searchInput;
 };
 
+const getBooks = async (search) => {
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=5`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data.items;
+};
+
+// build the HTML for book list from api response
 const buildBookHTML = async (searchInput) => {
   let bookListHTML = "";
   const searchResults = await getBooks(searchInput);
-  console.log(searchResults);
 
   for (let book of searchResults) {
     bookListHTML += `<li>${book.volumeInfo.title} By - ${book.volumeInfo.authors[0]}</li></br>`;
@@ -36,11 +34,18 @@ const buildBookHTML = async (searchInput) => {
   return bookListHTML;
 };
 
-document.getElementById("searchButton").addEventListener("click", () => {
-  getInput();
-});
-
+// render the booklist
 const render = async (searchInput) => {
   const innerHTML = await buildBookHTML(searchInput);
   searchResultsElement.innerHTML += innerHTML;
 };
+
+// function to handle search submit
+const searchRequest = () => {
+  render(getInput());
+};
+
+// event listener for search button
+document.getElementById("searchButton").addEventListener("click", () => {
+  searchRequest();
+});
