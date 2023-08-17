35b0e834cf388649f2053847e6eed25ffb2c1150
M. Ivkovic <m.ivkovic.dev@gmail.com>
2023-08-17 15:00:36 +0100

    input to search display implemented

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // build a quick test div to see if the app is working
-const app = document.getElementById("foundBooks");
+const searchResultsElement = document.getElementById("foundBooks");
 
 // test api call to google.books api
 
 const url = "https://www.googleapis.com/books/v1/volumes?q=";
 
 const getBooks = async (search) => {
+  console.log(url + search + "&maxResults=5");
   const res = await fetch(url + search + "&maxResults=5");
   const data = await res.json();
   return data.items;
@@ -14,13 +15,19 @@ const getBooks = async (search) => {
 // function to get user input and save as search string
 const getInput = () => {
   const searchInput = document.getElementById("searchInput").value;
-  console.log(searchInput);
-  return searchInput;
+  // verify input
+  if (searchInput === "") {
+    alert("Please enter a search term");
+    return;
+  }
+
+  render(searchInput);
 };
 
-const buildBookHTML = async () => {
+const buildBookHTML = async (searchInput) => {
   let bookListHTML = "";
-  const searchResults = await getBooks("author:Simon Reeve");
+  const searchResults = await getBooks(searchInput);
+  console.log(searchResults);
 
   for (let book of searchResults) {
     bookListHTML += `<li>${book.volumeInfo.title} By - ${book.volumeInfo.authors[0]}</li></br>`;
@@ -30,12 +37,10 @@ const buildBookHTML = async () => {
 };
 
 document.getElementById("searchButton").addEventListener("click", () => {
-  console.log("clicked");
+  getInput();
 });
 
-const render = async (element) => {
-  const innerHTML = await buildBookHTML();
-  element.innerHTML += innerHTML;
+const render = async (searchInput) => {
+  const innerHTML = await buildBookHTML(searchInput);
+  searchResultsElement.innerHTML += innerHTML;
 };
-
-render(app);
